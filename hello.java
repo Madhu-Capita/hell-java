b9ca916e3b6c691b72d8b85d19d7b97677f8c379
Madhu-Capita <56369413+Madhu-Capita@users.noreply.github.com>
2019-12-02 23:15:42 +0000

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,16 @@
+
+public class Hello {
+
+
+
+    public static void main(String[] args) {
+
+       System.out.println("Hello, World");
+
+        System.out.println("Triggering one new job from Pune");
+
+    }
+
+
+
+}
